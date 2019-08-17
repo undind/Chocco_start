e4eb2c2e79c07e0b06b41d2c709333cd39adda7e
Sergey Antonov <flyker777@yandex.ru>
2019-08-17 22:49:25 +0300

    GULP SASS

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const autoprefixer = require("autoprefixer");
+const concat = require('gulp-concat');
+const cssnano = require("cssnano");
+const postcss = require("gulp-postcss");
+const browsersync = require("browser-sync").create();
+
+function browserSync(done) {
+  browsersync.init({
+    server: {
+      baseDir: "./app/"
+    }
+  });
+  done();
+}
+
+function browserSyncReload(done) {
+  browsersync.reload();
+  done();
+}
+
+function styles() {
+  return gulp
+    .src("./app/scss/*.scss")
+    .pipe(concat('style.css'))
+    .pipe(sass({ outputStyle: "expanded" }))
+    .pipe(postcss([autoprefixer(), cssnano()]))
+    .pipe(gulp.dest('./app/css'))
+    .pipe(browsersync.stream());
+}
+
+function watchFiles() {
+  gulp.watch("./app/scss/**/*", browserSyncReload);
+  gulp.watch('./app/*.html', browserSyncReload);
+}
+
+gulp.task('default', gulp.parallel(watchFiles, browserSync));
