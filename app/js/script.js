b1c7d169d0eff14d1efd2748a07b4703d6d84e7e
Sergey Antonov <flyker777@yandex.ru>
2019-09-01 20:23:43 +0300

    fin slider

--- app/js/script.js
+++ app/js/script.js
@@ -38,3 +38,51 @@ hamburgerButton.addEventListener('click', function (e) {
     hideMenu();
    }
   });
+
+  slider();
+
+  function slider() { 
+    const sliderLeftButton = document.querySelector('.assortment__control--left');
+    const sliderRightButton = document.querySelector('.assortment__control--right');
+    const slider = document.querySelector('#slider');
+    const slides = document.querySelectorAll('.assortment__item');
+    const slide = document.querySelector('.assortment__item');
+
+    let minRight = 0;
+    let step = slide.offsetWidth;
+    let maxRight = (slides.length - 1) * step;
+    let currentRight = 0;
+
+    slider.style.right = currentRight;
+
+    function leftMove() {
+      if (currentRight > minRight) {
+        currentRight -= step;
+        slider.style.right = currentRight + 'px';
+      } else {
+        currentRight = maxRight;
+        slider.style.right = maxRight + 'px';
+      }
+    }
+
+    function rightMove() {
+      if (currentRight < maxRight) {
+        currentRight += step;
+        slider.style.right = currentRight + 'px';
+      } else {
+        currentRight = minRight;
+        slider.style.right = minRight + 'px';
+      }
+    }
+
+    sliderLeftButton.addEventListener('click', e => {
+      e.preventDefault();
+      leftMove();
+    });
+
+    sliderRightButton.addEventListener('click', e => {
+      e.preventDefault();
+      rightMove();
+    });
+
+   }
