4fc08f6da6044eec1f4e97619171373c0ee8fc0e
Sergey Antonov <flyker777@yandex.ru>
2019-09-08 17:21:32 +0300

    fin send form action and overlay

--- app/js/script.js
+++ app/js/script.js
@@ -1,3 +1,4 @@
+// Гамбургер меню
 const hamburgerMenu = document.querySelector('#hamburgerMenu');
 const hamburgerButton = document.querySelector('#hamburgerButton');
 
@@ -39,6 +40,8 @@ hamburgerButton.addEventListener('click', function (e) {
    }
   });
 
+
+// Слайдер ассортимент
 slider();
 
 function slider() { 
@@ -49,13 +52,13 @@ function slider() {
   const slide = document.querySelector('.assortment__item');
 
   let minRight = 0;
-  let step = slide.offsetWidth;
-  let maxRight = (slides.length - 1) * step;
   let currentRight = 0;
 
   slider.style.right = currentRight;
 
   function leftMove() {
+    let step = slide.offsetWidth;
+    let maxRight = (slides.length - 1) * step;
     if (currentRight > minRight) {
       currentRight -= step;
       slider.style.right = currentRight + 'px';
@@ -66,6 +69,8 @@ function slider() {
   }
 
   function rightMove() {
+    let step = slide.offsetWidth;
+    let maxRight = (slides.length - 1) * step;
     if (currentRight < maxRight) {
       currentRight += step;
       slider.style.right = currentRight + 'px';
@@ -85,29 +90,32 @@ function slider() {
     rightMove();
   });
 
-  };
+};
 
-  popup();
 
-  function popup() { 
-    const popupButton = document.querySelector('.assortment__popup-button');
+// Поппап
+popup();
+
+function popup() { 
+  const popupButton = document.querySelector('.assortment__popup-button');
+  
+  popupButton.addEventListener('click', e => {
+    e.preventDefault();
+
+    if (popupButton.classList.contains('active')) {
+      popupButton.classList.remove('active');
+    } else {
+      popupButton.classList.add('active');
+    }
     
-    popupButton.addEventListener('click', e => {
-      e.preventDefault();
+  });
+};
 
-      if (popupButton.classList.contains('active')) {
-        popupButton.classList.remove('active');
-      } else {
-        popupButton.classList.add('active');
-      }
-      
-    });
-  };
 
+// Акардеон
 accardeon();
 
 function accardeon() { 
-  // const menuItems = document.querySelectorAll('.menu__items');
   const menuLink = document.querySelectorAll('.menu__link');
   
   for (let i = 0; i < menuLink.length; i++) {
@@ -131,4 +139,74 @@ function accardeon() {
   };
 };
 
-  //menu__items--active
+// Модальное окно
+const template = document.querySelector('#overlayTemplate').innerHTML;
+const overlay = createOverlay(template);
+
+function createOverlay(template) { 
+  let fragment = document.createElement('div');
+  fragment.innerHTML = template;
+
+  const overlayElement = fragment.querySelector('.overlay');
+  const contentElement = fragment.querySelector('.overlay__content');
+  const buttonElement = fragment.querySelector('.overlay__btn');
+
+  fragment = null;
+
+  overlayElement.addEventListener('click', e => {
+    if (e.target === overlayElement) {
+      buttonElement.click();
+    }
+  });
+
+  buttonElement.addEventListener('click', e => {
+    e.preventDefault();
+    document.body.removeChild(overlayElement);
+    document.body.classList.remove('body-fix');
+  });
+
+  return {
+    open() {
+      document.body.appendChild(overlayElement);
+      document.body.classList.add('body-fix');
+    },
+    close() {
+      buttonElement.click();
+    },
+    setContent(content) {
+      contentElement.innerHTML = content;
+    }
+  }
+};
+
+// Обработка формы
+const myForm = document.querySelector('#myForm');
+let sendFormButton = document.querySelector('#sendForm');
+
+sendFormButton.addEventListener('click', e => {
+  e.preventDefault();
+
+  let formData = new FormData();
+
+  formData.append('name', myForm.elements.name.value);
+  formData.append('phone', myForm.elements.phone.value);
+  formData.append('comment', myForm.elements.comment.value);
+  formData.append('to', 'to@mail.com');
+
+  const xhr = new XMLHttpRequest();
+
+  xhr.responseType = 'json';
+  xhr.open('POST', 'https://webdev-api.loftschool.com/sendmail');
+  xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+  xhr.send(formData);
+
+  xhr.addEventListener('load', function() {
+    if (xhr.status >= 400) {
+      overlay.open();
+      overlay.setContent('Что-то пошло не так!');
+    } else {
+      overlay.open();
+      overlay.setContent('Сообщение отправлено');
+    }
+  });
+});
