fbfeea63a6d31408b50cc96c6b396b87b06f19c3
Sergey Antonov <flyker777@yandex.ru>
2019-09-10 22:43:54 +0300

    fin sliderShow

--- app/js/script.js
+++ app/js/script.js
@@ -139,6 +139,41 @@ function accardeon() {
   };
 };
 
+// Слайдшоу комментарии
+const reviewsControls = document.querySelectorAll('.reviews__item');
+const reviews = document.querySelectorAll('.reviews__content');
+let currentReview = 0;
+let showInterval = setInterval(nextReview, 4000);
+
+function nextReview() { 
+  goToReviews(currentReview + 1);
+};
+
+for (let i = 0; i < reviewsControls.length; i++) {
+  const reviewsControl = reviewsControls[i];
+  reviewsControl.setAttribute('data-index', [i]);
+  reviewsControl.addEventListener('click', function() {
+    clearInterval(showInterval);
+    showInterval = setInterval(nextReview, 4000);
+    this.classList.add('active');
+    goToReviews(reviewsControl.getAttribute('data-index'));
+  });
+};
+
+function goToReviews(n) { 
+  reviews[currentReview].classList.remove('active');
+  reviews[currentReview].style.display = 'none';
+  reviewsControls[currentReview].classList.remove('active');
+
+  currentReview = (n + reviews.length) % reviews.length;
+  reviews[currentReview].style.display = 'flex';
+  reviewsControls[currentReview].classList.add('active');
+
+  setTimeout(() => {
+    reviews[currentReview].classList.add('active');
+  }, 100);
+};
+
 // Модальное окно
 const template = document.querySelector('#overlayTemplate').innerHTML;
 const overlay = createOverlay(template);
